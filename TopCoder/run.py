4e77cfe8866d74132cc9d1207ab4ef793734f676
Junqiao Wang <junqiao.wang@hulu.com>
2016-02-29 21:57:53 +0800

    update run.py.

--- TopCoder/run.py
+++ TopCoder/run.py
@@ -1,37 +1,39 @@
 import io
 import os
+import subprocess
 
 file_name = 'srm.cpp'
 run_number = 0
 
 def compile_run():
     global run_number
-    print 'Run round ', run_number
+    print '\n\n\n\n\n', 'Run round ', run_number
     run_number = run_number + 1
-    stream = os.popen("g++ -o aaa srm.cpp")
-    output = stream.read()
-    print output
-    if (len(output) == 0):
-        stream = os.popen("./aaa")
-        print stream.read()
+    p = subprocess.Popen(['g++', '-o', 'aaa', 'srm.cpp'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    print 'out: ', out
+    print 'err: ', err
+    if (err == ''):
+        subprocess.check_call('./aaa')
 
 if __name__ == '__main__':
     run_number = 0
     lines = []
     while True:
-        with io.open(file_name, 'r') as f:
-            tmp_lines = f.readlines() 
-        if len(lines) != len(tmp_lines):
-            print 'different rows'
-            lines = tmp_lines
-            compile_run()
-            continue
-        diff = False
-        for i in range(len(lines)):
-            if lines[i] != tmp_lines[i]:
-                diff = True
-        if (diff):
-            print 'different content'
-            lines = tmp_lines
-            compile_run()
+        try:
+            with io.open(file_name, 'r') as f:
+                tmp_lines = f.readlines() 
+            if len(lines) != len(tmp_lines):
+                lines = tmp_lines
+                compile_run()
+                continue
+            diff = False
+            for i in range(len(lines)):
+                if lines[i] != tmp_lines[i]:
+                    diff = True
+            if (diff):
+                lines = tmp_lines
+                compile_run()
+                continue
+        except Exception as e:
             continue
