e37d9d306cb1e38db39fc20b2363a18ace24e407
Junqiao Wang <junqiao.wang@hulu.com>
2016-03-01 22:09:30 +0800

    update run.py.

--- TopCoder/run.py
+++ TopCoder/run.py
@@ -7,14 +7,16 @@ run_number = 0
 
 def compile_run():
     global run_number
-    print '\n\n\n\n\n', 'Run round ', run_number
+    print '\n\n', 'Run round ', run_number
     run_number = run_number + 1
-    p = subprocess.Popen(['g++', '-o', 'aaa', 'srm.cpp'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(['g++', '-o', 'aaa', 'srm.cpp'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
-    print 'out: ', out
-    print 'err: ', err
+
     if (err == ''):
+        print 'Compiled successfully & Run...\n'
         subprocess.check_call('./aaa')
+    else:
+        print 'Error:\n', err
 
 if __name__ == '__main__':
     run_number = 0
